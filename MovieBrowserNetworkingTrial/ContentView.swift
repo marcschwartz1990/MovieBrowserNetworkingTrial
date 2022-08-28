657b19c84d191ef5973ae33cb96cdd12efeb4600
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-28 15:05:28 -0400

    Added infoPageView and searchResultView

--- MovieBrowserNetworkingTrial/ContentView.swift
+++ MovieBrowserNetworkingTrial/ContentView.swift
@@ -15,7 +15,12 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(movieListVM.movies, id: \.id) { movie in
-                Text(movie.title)
+                NavigationLink {
+                    infoPageView(movie: movie)
+                } label: {
+                    searchResultView(movie: movie)
+                }
+                .navigationBarTitle("Movie Search", displayMode: .inline)
             }.listStyle(.plain)
                 .searchable(text: $searchText)
                 .onChange(of: searchText) { value in
@@ -26,9 +31,61 @@ struct ContentView: View {
                     }
                 }
             
-                .navigationTitle("Movies")
+                .navigationTitle("Movie Search")
+                .navigationBarTitleDisplayMode(.inline)
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct searchResultView: View {
+    let movie: MovieViewModel
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            
+        Text(movie.title)
+            .font(.system(size: 22))
+            
+            HStack {
+                Text("\(movie.popularity)")
+                    .font(.system(size: 10))
+                    .fixedSize(horizontal: true, vertical: false)
+            }
+            
+            
+        Text(movie.releaseDate)
+            .font(.system(size: 14))
+            .foregroundColor(.gray)
+        }
+    }
+}
+
+struct infoPageView: View {
+    let movie: MovieViewModel
+    
+    var body: some View {
+        VStack {
+            Text(movie.title)
+                .font(.system(size: 36))
+            Text("Release Date: \(movie.releaseDate)")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+                .padding(20)
+            
+            HStack {
+                AsyncImage(url: movie.posterPath!) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 100, height: 200)
+                
+                Text(movie.overview)
+            }
+            .padding(20)
+        }
+        Spacer()
     }
 }
 
